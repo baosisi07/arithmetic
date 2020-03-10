efaef3c8a20bf95b48415bf8c61332699664951c
baoss <18018021075@163.com>
2020-03-10 14:49:50 +0800

    链表

--- linked_list/linked_list1.js
+++ linked_list/linked_list1.js
@@ -1,3 +1,11 @@
+/**
+ * 1) 单链表反转
+ * 2) 链表中环的检测
+ * 3) 两个有序的链表合并
+ * 4) 删除链表倒数第n个结点
+ * 5) 求链表的中间结点
+ *
+ */
 class Node {
     constructor(ele) {
         this.element = ele
@@ -10,77 +18,9 @@ class LinkedList {
         this.head = new Node('head')
     }
 
-    append(ele) {
-        const newNode = new Node(ele)
-        let currentNode = this.head
-        while(currentNode.next) {
-            currentNode = currentNode.next
-        }
-        currentNode.next = newNode
-    }
-    display() {
-        let currentNode = this.head.next
-        while(currentNode) {
-            console.log(currentNode.element)
-            currentNode = currentNode.next
-        }
-    }
-    findByValue(ele) {
-        let currentNode = this.head.next
-        while(currentNode !== null && currentNode.element !== ele) {
-            currentNode = currentNode.next
-        }
-        console.log(currentNode)
-        return currentNode === null ? -1 : currentNode
-    }
-    findByIndex(i) {
-        let currentNode = this.head.next
-        let index = 0
-        while(currentNode !==null && index !== i) {
-            currentNode = currentNode.next
-            index++
-        }
-        console.log(currentNode)
-        return currentNode === null ? -1 : currentNode
-    }
-    insert(newELe,oldEle) {
-        const currentNode = this.findByValue(oldEle)
-        const newNode = new Node(newELe)
-        if(currentNode.next) {
-            newNode.next = currentNode.next
-            currentNode.next = newNode
-        }
-        else if(currentNode.next === null) {
-            currentNode.next = newNode
-        }
+    reverseList() {
 
-    }
-    findPre(ele) {
-        let currentNode = this.head
-        while (currentNode.next !== null && currentNode.next.element !== ele) {
-            currentNode = currentNode.next
-        }
-        console.log(currentNode)
-        return currentNode === null ? -1 : currentNode
-    }
-    remove(ele) {
-        const preNode = this.findPre(ele)
-        if(preNode === -1) {
-            console.log('未找到元素')
-            return 
-        }
-        preNode.next = preNode.next.next
     }
 }
 
-list = new LinkedList()
-list.append('sisi')
-list.append('nianwen')
-list.findByIndex(0)
-list.findByValue('sisi')
-list.insert('hh','sisi')
-list.insert('nw', 'nianwen')
-console.log('preNode')
-list.findPre('hh')
-list.remove('sisi')
-list.display()
+
